6a7c0dd979c8194b0fc0800c06aff653d2dd440f
Your Namegit <you@example.com>
2021-08-13 15:53:11 -0300

    data-binding update

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 function AppViewModel() {
-    this.firstName = ko.observable("First name");
-    this.secondName = ko.observable("Second name");
+    this.firstName = ko.observable("Enter your first name");
+    this.secondName = ko.observable("Enter your second name");
 
     this.fullName = ko.computed(function() {
         return this.firstName() + " " + this.secondName();
     }, this);
+
 }
 
 ko.applyBindings(new AppViewModel());
