7cfbe2577e11cac73322bfb312a30cd445125de2
Your Namegit <you@example.com>
2021-08-16 16:37:16 -0300

    observable and computed

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
+// This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
 function AppViewModel() {
-    this.firstName = ko.observable("Enter your first name");
-    this.secondName = ko.observable("Enter your second name");
-
+    this.firstName = ko.observable("Bert");
+    this.lastName = ko.observable("Bertington");
     this.fullName = ko.computed(function() {
-        return this.firstName() + " " + this.secondName();
+        return this.firstName() + " " + this.lastName();
     }, this);
-
+    this.capitalizeLastName = function() {
+        var currentVal = this.lastName(); // Read the current value
+        this.lastName(currentVal.toUpperCase()); //Write back a modified value
+    };
 }
 
+// Activates knockout.js
 ko.applyBindings(new AppViewModel());
