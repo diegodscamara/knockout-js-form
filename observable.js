c524cce15c7048f25430f6313ddc12cc7dacb51a
Your Namegit <you@example.com>
2021-08-18 17:32:14 -0300

    observables

---
+++ observable.js
@@ -0,0 +1,17 @@
+function MyViewModel() {
+    this.a = ko.observable(10);
+    this.b = ko.observable(40);
+
+    this.totalAvg = ko.computed(function() {
+
+        if (typeof(this.a()) !== "number" || typeof(this.b()) !== "number") {
+            this.a(Number(this.a())); //convert string to Number
+            this.b(Number(this.b())); //convert string to Number
+        }
+
+        total = (this.a() + this.b()) / 2;
+        return total;
+    }, this);
+}
+
+ko.applyBindings(new MyViewModel());
